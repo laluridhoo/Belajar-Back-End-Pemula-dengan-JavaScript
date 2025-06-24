8b966019846208a2ea4d330e87e745b3f957e8e6
laluridhoo <ridhobingkir@gmail.com>
2025-06-24 11:28:05 +0800

    Kriteria 7 : API dapat menghapus buku

--- notes-app/src/handler.js
+++ notes-app/src/handler.js
@@ -160,9 +160,33 @@ const editBookByIdHandler = (request, h) => {
     .code(200);
 };
 
+const deleteBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index === -1) {
+    return h
+      .response({
+        status: "fail",
+        message: "Buku gagal dihapus. Id tidak ditemukan",
+      })
+      .code(404);
+  }
+
+  books.splice(index, 1);
+
+  return h
+    .response({
+      status: "success",
+      message: "Buku berhasil dihapus",
+    })
+    .code(200);
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
   editBookByIdHandler,
+  deleteBookByIdHandler,
 };
