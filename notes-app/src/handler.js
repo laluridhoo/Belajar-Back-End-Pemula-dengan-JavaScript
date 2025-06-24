ee9179d8d5266f9c800a8be812a1537d5b03e6d9
laluridhoo <ridhobingkir@gmail.com>
2025-06-24 11:22:48 +0800

    Kriteria 5 : API dapat menampilkan detail buku

--- notes-app/src/handler.js
+++ notes-app/src/handler.js
@@ -82,4 +82,27 @@ const getAllBooksHandler = (request, h) => {
     .code(200);
 };
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+  const book = books.find((b) => b.id === bookId);
+
+  if (book) {
+    return h
+      .response({
+        status: "success",
+        data: {
+          book,
+        },
+      })
+      .code(200);
+  }
+
+  return h
+    .response({
+      status: "fail",
+      message: "Buku tidak ditemukan",
+    })
+    .code(404);
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
